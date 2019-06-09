1a46d70cb13ca70dd50effae262de6021c1ac47b
Daria <demidova.daria.pavlovna@gmail.com>
2019-06-09 15:33:38 +0300

    Рабочая статистика.

--- js/stat.js
+++ js/stat.js
@@ -1,61 +1,93 @@
 'use strict';
 
-var CLOUD_WIDTH = 420; // ширина облака
-var CLOUD_HEIGHT = 270; // высота облака
-var cloudColor = '#fff'; // цвет облака
-var cloudShadowColor = 'rgba(0, 0, 0, 0.7)'; // цвет тени
-var CLOUD_X = 100; // координата по осии Х облака
-var CLOUD_Y = 10; // координата по осии Y облака
-var BARGRAPH_FONT = '16px PT Mono'; // параметры шрифта для текста поздравления
-var FONT_COLOR = 'rgba(0, 0, 0, 1)'; // цвет шрифта
-var BARGRAPH_CONGRATULATION = ['Ура, вы победили!', 'Список результатов:'];
-var BARGRAPH_HEIGHT = 150; // высота гистограммы
-var BARGRAPH_WIDTH = 40; // ширина гистограммы
-// var COLUMN_SPACING = BARGRAPH_WIDTH + 50; // расстояние между столбцами
-var bargraphColor; // цвет столбцов гистограммы
-var COLUMN_X = 120;
-var COLUMN_Y = 270;
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_COLOR = '#fff';
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
 
-// Функция, рисующая облако
+var GAP_SIZE = 10;
+
+var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
+var SHADOW_X = CLOUD_X + GAP_SIZE;
+var SHADOW_Y = CLOUD_Y + GAP_SIZE;
+
+var STATISTICS_TEXT_COLOR = '#283442';
+var STATISTICS_TEXT_FONT = '16px PT Mono';
+var STATISTICS_FONT_HEIGHT = 16;
+
+var TEXT_CONGRATULATIONS = ['Ура, вы победили!', 'Список результатов:'];
+var TEXT_X_OFFSET = 20;
+var TEXT_X_BEGIN = CLOUD_X + TEXT_X_OFFSET;
+var TEXT_Y_OFFSET = 40;
+var TEXT_Y_BEGIN = CLOUD_Y + TEXT_Y_OFFSET;
+var TEXT_Y_DISTANCE = 20;
+
+var COLUMN_DISTANCE = 50;
+var COLUMN_X_BEGIN = 120;
+var COLUMN_Y_BEGIN = 250;
+var COLUMN_HEIGHT = 150;
+var COLUMN_WIDTH = 40;
+var COLUMN_WINNER_TEXT = 'Вы';
+var COLUMN_WINNER_COLOR = 'rgba(255, 0, 0, 1)';
+
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-// Функция, которая выводит текст приветствия
-var getStatisticsText = function (ctx) {
-  ctx.fillStyle = FONT_COLOR;
-  ctx.font = BARGRAPH_FONT;
-  var textY = 40;
-  for (var i = 0; i < BARGRAPH_CONGRATULATION.length; i++) {
-    ctx.fillText(BARGRAPH_CONGRATULATION[i], CLOUD_X + 20, CLOUD_Y + textY);
-    textY += 20;
+var renderStatisticsText = function (ctx) {
+  ctx.fillStyle = STATISTICS_TEXT_COLOR;
+  ctx.font = STATISTICS_TEXT_FONT;
+  var y = TEXT_Y_BEGIN;
+  for (var i = 0; i < TEXT_CONGRATULATIONS.length; i++) {
+    ctx.fillText(TEXT_CONGRATULATIONS[i], TEXT_X_BEGIN, y);
+    y += TEXT_Y_DISTANCE;
   }
 };
 
-// Функция, которая определяет цвет столбцов гистаграммы
-var getBargraphColor = function (name) {
-  if (name === 'Вы') {
-    bargraphColor = 'rgba(255, 0, 0, 1)';
-  } else {
-    bargraphColor = 'rgba(255, 0, 0, ' + Math.random() + ')';
-  }
-  return bargraphColor;
+var getBarColor = function (name) {
+  return name === COLUMN_WINNER_TEXT
+    ? COLUMN_WINNER_COLOR
+    : 'rgba(0, 100, 0, ' + Math.random() + ')';
 };
 
 // Функция, которая рисует столбцы гистограммы
-function drawBargraph(ctx, x, y, height, color) {
+var renderBar = function renderBar(ctx, x, y, height, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x, y, BARGRAPH_WIDTH, -height);
-}
+  ctx.fillRect(x, y, COLUMN_WIDTH, -height);
+};
 
-window.renderStatistics = function (ctx, names /* , times */) {
-  renderCloud(ctx, CLOUD_X + 10, CLOUD_Y + 10, cloudShadowColor); // рисуем тень облака
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, cloudColor); // рисуем белое облако
-  getStatisticsText(ctx); // выводим текст поздравления
-  var color;
+var getMaxTime = function (times) {
+  var maxTime = times[0];
+
+  for (var i = 0; i < times.length; i++) {
+    if (times[i] > maxTime) {
+      maxTime = times[i];
+    }
+  }
+  return maxTime;
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, SHADOW_X, SHADOW_Y, SHADOW_COLOR);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
+  renderStatisticsText(ctx);
+
+  var columnHeight;
+  var columnX = COLUMN_X_BEGIN;
+  var maxTime = getMaxTime(times);
+  var barScale = COLUMN_HEIGHT / maxTime;
+  var dist = 0;
+
   for (var i = 0; i < names.length; i++) {
-    color = getBargraphColor(names[i]);
-    drawBargraph(ctx, COLUMN_X, COLUMN_Y, BARGRAPH_HEIGHT, color);
+    ctx.fillStyle = STATISTICS_TEXT_COLOR;
+    ctx.font = STATISTICS_TEXT_FONT;
+    ctx.fillText(Math.round(times[i]), TEXT_X_BEGIN + dist, CLOUD_HEIGHT - (barScale * times[i]) - STATISTICS_FONT_HEIGHT * 1.5);
+    ctx.fillText(names[i], (TEXT_X_BEGIN + dist), CLOUD_HEIGHT);
+    dist += COLUMN_WIDTH + COLUMN_DISTANCE;
+    columnHeight = times[i] * barScale;
+    renderBar(ctx, columnX, COLUMN_Y_BEGIN, columnHeight, getBarColor(names[i]));
+    columnX += COLUMN_WIDTH + COLUMN_DISTANCE;
   }
 };
